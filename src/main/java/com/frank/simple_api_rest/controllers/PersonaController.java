20776de2b2ebc64aa77af580649b4db0959dbcca
Franco Inzerillo <83378749+Frank076-web@users.noreply.github.com>
2022-11-02 15:54:39 -0300

    Controller persona (#2)
    * Added response hanlder for data of Persona
    
    * Added overloading method for PersonaResponseEntity and added getAll method for controller of Persona
    
    * Added getOne method in PersonaController
    
    * Added save method for PersonaController
    
    * Added update method for PersonaController
    
    * Added delete method for PersonaController
    
    * Added annotation path variable forgotten in delete method for PersonaController
    
    Co-authored-by: Franco <francoeinzerillo@gmail.com>

---
+++ src/main/java/com/frank/simple_api_rest/controllers/PersonaController.java
@@ -0,0 +1,71 @@
+package com.frank.simple_api_rest.controllers;
+
+import com.frank.simple_api_rest.entities.Persona;
+import com.frank.simple_api_rest.responses.PersonaResponseEntity;
+import com.frank.simple_api_rest.services.Persona.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/personas")
+public class PersonaController {
+
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping("")
+    public ResponseEntity<Object> getAll(){
+        try {
+            List<Persona> personas = personaService.findAll();
+            return PersonaResponseEntity.buildResponse(HttpStatus.OK, personas, "Encontrado");
+        } catch (Exception e) {
+            return PersonaResponseEntity.buildResponse(HttpStatus.NOT_FOUND, "Error, no se pudo encontrar. Por favor intente de nuevo mas tarde");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable Long id) {
+        try {
+            Persona persona = personaService.findById(id);
+            return PersonaResponseEntity.buildResponse(HttpStatus.OK, persona, "Encontrado");
+        } catch (Exception e) {
+            return PersonaResponseEntity.buildResponse(HttpStatus.NOT_FOUND, "Error, no se pudo encontrar la persona solicitada. Por favor intente de nuevo mas tarde");
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> save(@RequestBody Persona entity) {
+        try {
+            Persona persona = personaService.save(entity);
+            return PersonaResponseEntity.buildResponse(HttpStatus.CREATED, persona, "Guardado correctamente");
+        } catch (Exception e) {
+            return PersonaResponseEntity.buildResponse(HttpStatus.BAD_REQUEST, "Error, no se pudo guardar. Por favor intente de nuevo mas tarde");
+        }
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Object> update(@RequestBody Persona entity) {
+        try {
+            Persona persona = personaService.update(entity);
+            return PersonaResponseEntity.buildResponse(HttpStatus.OK, persona, "Actualizado correctamente");
+        } catch (Exception e) {
+            return PersonaResponseEntity.buildResponse(HttpStatus.BAD_REQUEST, "Error, no se pudo actualizar. Por favor intente de nuevo mas tarde");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        try {
+            personaService.delete(id);
+            return PersonaResponseEntity.buildResponse(HttpStatus.OK, "Eliminado correctamente");
+        } catch (Exception e) {
+            return PersonaResponseEntity.buildResponse(HttpStatus.BAD_REQUEST, "Error, no se pudo eliminar. Por favor intente de nuevo mas tarde");
+        }
+    }
+
+}
