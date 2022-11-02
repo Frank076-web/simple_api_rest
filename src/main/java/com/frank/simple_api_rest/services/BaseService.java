ccd15d1d0628d7e75b8775eb0ef26c730f3af050
Franco <francoeinzerillo@gmail.com>
2022-11-02 10:35:43 -0300

    Added base service interface

---
+++ src/main/java/com/frank/simple_api_rest/services/BaseService.java
@@ -0,0 +1,16 @@
+package com.frank.simple_api_rest.services;
+
+import java.util.List;
+
+public interface BaseService<T> {
+
+    List<T> findAll() throws Exception;
+
+    T findById(Long id) throws Exception;
+
+    T save(T entitie) throws Exception;
+
+    T update (Long id, T entitie) throws Exception;
+
+    Boolean delete(Long id) throws  Exception;
+}
