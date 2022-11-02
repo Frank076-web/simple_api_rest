dccb55e3113206b7cae5af2b4dabd1e978adb17c
Franco <francoeinzerillo@gmail.com>
2022-11-02 10:38:55 -0300

    Added Interface Persona service and class Persona service with overrideMethods

---
+++ src/main/java/com/frank/simple_api_rest/services/Persona/PersonaService.java
@@ -0,0 +1,33 @@
+package com.frank.simple_api_rest.services.Persona;
+
+import com.frank.simple_api_rest.entities.Persona;
+
+import java.util.List;
+
+public class PersonaService implements IPersonaService{
+
+    @Override
+    public List<Persona> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona save(Persona entitie) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona update(Long id, Persona entitie) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) throws Exception {
+        return null;
+    }
+}
