1e5c2a96123779609719955f6ae9f38a9d978c13
Franco <francoeinzerillo@gmail.com>
2022-11-02 10:26:50 -0300

    Added revision and listener for audited entities

---
+++ src/main/java/com/frank/simple_api_rest/config/CustomRevisionListener.java
@@ -0,0 +1,12 @@
+package com.frank.simple_api_rest.config;
+
+import com.frank.simple_api_rest.entities.audit.Revision;
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object revisionEntity) {
+        final Revision revision = (Revision) revisionEntity;
+    }
+}
