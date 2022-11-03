21618a0b515f9b76d2598ca562f1a6c7d9939f7d
Franco <francoeinzerillo@gmail.com>
2022-11-03 18:54:18 -0300

    Adding new querys and implementing

--- src/main/java/com/frank/simple_api_rest/services/Persona/PersonaService.java
+++ src/main/java/com/frank/simple_api_rest/services/Persona/PersonaService.java
@@ -3,5 +3,10 @@ package com.frank.simple_api_rest.services.Persona;
 import com.frank.simple_api_rest.entities.Persona;
 import com.frank.simple_api_rest.services.BaseService;
 
+import java.util.List;
+
 public interface PersonaService extends BaseService<Persona, Long> {
+
+    List<Persona> search(String filtro) throws Exception;
+
 }
