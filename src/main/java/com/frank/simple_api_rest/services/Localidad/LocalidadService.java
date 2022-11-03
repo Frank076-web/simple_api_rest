7e0eba0df7aebe127b53f6d2b27ac589f517a7dd
Franco <francoeinzerillo@gmail.com>
2022-11-03 17:53:36 -0300

    Added service for Localidad

---
+++ src/main/java/com/frank/simple_api_rest/services/Localidad/LocalidadService.java
@@ -0,0 +1,9 @@
+package com.frank.simple_api_rest.services.Localidad;
+
+import com.frank.simple_api_rest.entities.Localidad;
+import com.frank.simple_api_rest.services.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LocalidadService extends BaseService<Localidad, Long> {
+}
